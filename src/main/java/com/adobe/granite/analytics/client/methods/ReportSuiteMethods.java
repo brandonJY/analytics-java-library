ae6b88f0e3e62ecea8dbf8c1498f57116379ab49
Brandon Jiang <brandon.jiang.p@outlook.com>
2017-01-09 15:47:26 +0800

    set variable method

--- src/main/java/com/adobe/granite/analytics/client/methods/ReportSuiteMethods.java
+++ src/main/java/com/adobe/granite/analytics/client/methods/ReportSuiteMethods.java
@@ -9,10 +9,14 @@ import java.util.List;
 
 import com.adobe.granite.analytics.client.AnalyticsClient;
 import com.adobe.granite.analytics.client.domain.CompanyReportSuites;
+import com.adobe.granite.analytics.client.domain.Evar;
+import com.adobe.granite.analytics.client.domain.Event;
+import com.adobe.granite.analytics.client.domain.Prop;
 import com.adobe.granite.analytics.client.domain.ReportSuiteEvars;
 import com.adobe.granite.analytics.client.domain.ReportSuiteEvents;
 import com.adobe.granite.analytics.client.domain.ReportSuiteProps;
 import com.google.gson.reflect.TypeToken;
+import static com.adobe.granite.analytics.client.JsonUtil.GSON;
 
 public class ReportSuiteMethods {
 
@@ -39,5 +43,19 @@ public class ReportSuiteMethods {
 	public List<ReportSuiteEvents> getEvents(String... rsid) throws IOException {
 		final Type type = new TypeToken<List<ReportSuiteEvents>>() {}.getType();
 		return client.callMethod("ReportSuite.GetEvents", o("rsid_list", rsid), type);
+	}
+	
+	public Boolean setEvars(List<Evar> varList, List<String> reportSuiteList) throws IOException {
+		String jsonStr=GSON.toJson(o("evars",varList,"rsid_list", reportSuiteList));
+		return Boolean.parseBoolean(client.callMethod("ReportSuite.SaveEvars", jsonStr));
+	}
+	
+	public Boolean setProps(List<Prop> varList, List<String> reportSuiteList) throws IOException {
+		String jsonStr=GSON.toJson(o("props",varList,"rsid_list", reportSuiteList));
+		return Boolean.parseBoolean(client.callMethod("ReportSuite.SaveProps", jsonStr));
+	}
+	public Boolean setEvents(List<Event> varList, List<String> reportSuiteList) throws IOException {
+		String jsonStr=GSON.toJson(o("events",varList,"rsid_list", reportSuiteList));
+		return Boolean.parseBoolean(client.callMethod("ReportSuite.SaveEvents", jsonStr));
 	}
 }
